3aae50e323d91d38fb677472113fe7fe8d40480e
krzhnaa <krzna.ydv@gmail.com>
2026-03-18 01:50:31 +0530

    connect frontend to deployed backend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,7 +6,7 @@ import ResultCard from "../components/ResultCard";
 import LoadingOverlay from "../components/LoadingOverlay";
 import Toast from "../components/Toast";
 
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://signature-verification-hcub.onrender.com";
 
 function Home() {
   const [users, setUsers] = useState([]);
